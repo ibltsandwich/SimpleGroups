87ca570adb9237d408ce711204f444f59e515d40
Brian Lee <ibltsandwich@gmail.com>
2019-09-12 16:34:22 -0400

    Build out user and user auth

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+  def create 
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+    else
+      render json: ['Invalid username or password'], status: 422
+    end
+  end
+
+  def destroy
+    @user = current_user
+    if @user
+      logout
+    else
+      render json: ['Nobody signed in'], status: 404
+    end
+  end
+
+end
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const webpack = require("webpack");
+
+let plugins = [];
+const devPlugins = [];
+
+const prodPlugins = [
+    new webpack.DefinePlugin({
+        'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+        }
+    })
+];
+
+plugins = plugins.concat(
+    process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+)
+
+module.exports = {
+    context: __dirname,
+    entry: '',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    }
+};

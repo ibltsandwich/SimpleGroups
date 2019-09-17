03ea358a1eeda49b4c26b60f1cd665607c46e575
Brian Lee <ibltsandwich@gmail.com>
2019-09-17 04:49:24 -0400

    Change routes and html

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/index'
   root to: 'static_pages#index'
+  
   namespace :api, defaults: {format: :json} do
     resources :users, only: [:create, :show] do
       resources :memberships
